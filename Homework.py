a7f7cc642661f917faf2c729bda36137412712e4
Rajkrishna <157878715+RajKrishna25@users.noreply.github.com>
2024-01-30 11:48:34 +0530

    Add files via upload

---
+++ Homework.py
@@ -0,0 +1,12 @@
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+gender = input("Enter your gender (M/F): ")
+weight = float(input("Enter your weight (in kg): "))
+
+
+print("Output:-")
+print("You name is:-", name)
+print("your age is:- ", age, "years")
+print("Your gender is:-", gender)
+print("Your weight is:", weight, "kgs")
