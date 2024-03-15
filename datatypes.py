8c610a01ad2c5782f9cf2378143e8b4a68e1db87
Rajkrishna <157878715+RajKrishna25@users.noreply.github.com>
2024-03-15 14:28:49 +0530

    Add files via upload

--- datatypes.py
+++ datatypes.py
@@ -1,51 +1,9 @@
-name=input("Enter your name:") #string
-age=int(input("Enter your age:"))#string
-gender=input("Enter your gender:")#string
-weight=float(input("Enter your weight:"))#string
+# String to Integer: 
+String = input("Enter a number as string: ")
+Integer = int(String)
+print(Integer)
 
-
-print("Your name is :",name,type(name))
-print("Your age is :",age,type(age))
-print("Your gender is :",gender,type(gender))
-print("Your weight is :",weight,type(weight))#45.45
-weight=int(weight)
-print("Your weight is :",weight,type(weight))
-#float>int
-
-# # single line comment
-# """
-# multi line comment
-# """
-
-# write a program to get user input of their age and height and weight and calculate their bmi
-
-# 
-a=20
-b=6
-# arithmetic
-print("sum",a+b)
-print("sub",a-b)
-print("multiply",a*b)
-print("exponentiation",a**b)
-print("floor division",a//b)
-print("modulus",a%b)
-
-# comparison
-print("greater than" ,a>b)
-print("less than" ,a<b)
-# print("geater than" ,a>b)
-# print("geater than" ,a>b)
-print("equal " ,a==b)
-print("not equal " ,a!=b)
-
-# logical
-x = 2
-
-print(x > 3 and x < 10)
-x = 5
-
-print(x > 3 or x < 4)
-x = 5
-
-print(not(x > 3 and x < 10))
-# exp= a>b and a==b #true and true -> true
+# Integer to float
+Integer = int(input("Enter a number as Integer: "))
+Float = float(Integer)
+print(Float)
